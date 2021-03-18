54433c9ae327b1bc174ae87e3e6a4ded3f5825b1
smassarsky <seth.massarsky@gmail.com>
2021-03-18 18:46:36 -0400

    hosting app

--- src/components/teamShow/ShowCodeModal.js
+++ src/components/teamShow/ShowCodeModal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Modal from 'react-bootstrap/Modal'
 import InputGroup from 'react-bootstrap/InputGroup'
@@ -8,34 +8,45 @@ import FormControl from 'react-bootstrap/FormControl'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClipboard } from '@fortawesome/free-solid-svg-icons'
 
-const ShowCodeModal = props => {
-  console.log(props)
-  return (
-    <Modal show={props.show} onHide={props.hideModal} centered="true" >
-      <Modal.Body>
-        <h6>Copy this code and send it to {props.player ? props.player.name : null}</h6>
-        <InputGroup>
-          <FormControl
-            type="text"
-            readOnly={true}
-            value={props.loading || !props.player ? "Loading..." : props.player.playerCode.code}
-          />
-          <InputGroup.Append>
-            <Button
-              type="button"
-              variant="outline-secondary"
-              onClick={handleCopy}
-            >
-              <FontAwesomeIcon icon={faClipboard} />
-            </Button>
-          </InputGroup.Append>
-        </InputGroup>
-      </Modal.Body>
-    </Modal>
-  )
+class ShowCodeModal extends Component {
 
-  function handleCopy() {
-    navigator.clipboard.writeText(props.player.playerCode.code)
+  constructor(props) {
+    super(props)
+    this.playerCode = React.createRef()
+  }
+
+  handleCopy = e => {
+    this.playerCode.select()
+    document.execCommand('copy')
+    e.target.focus()
+  }
+
+  render() {
+    console.log(props)
+    return (
+      <Modal show={props.show} onHide={props.hideModal} centered="true" >
+        <Modal.Body>
+          <h6>Copy this code and send it to {props.player ? props.player.name : null}</h6>
+          <InputGroup>
+            <FormControl
+              type="text"
+              readOnly={true}
+              value={props.loading || !props.player ? "Loading..." : props.player.playerCode.code}
+              ref={this.playerCode}
+            />
+            <InputGroup.Append>
+              <Button
+                type="button"
+                variant="outline-secondary"
+                onClick={handleCopy}
+              >
+                <FontAwesomeIcon icon={faClipboard} />
+              </Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Modal.Body>
+      </Modal>
+    )
   }
 }
 
